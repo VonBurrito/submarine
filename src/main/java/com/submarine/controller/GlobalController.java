bdeb9f801b02ab5fbde408f5c54ad1b131ce23ad
VonBurrito <hamza.amentag@hotmail.com>
2022-03-17 00:32:59 +0100

    secnd commit

---
+++ src/main/java/com/submarine/controller/GlobalController.java
@@ -0,0 +1,10 @@
+package com.submarine.controller;
+
+/**
+ * @author Hamza Amentag
+ * @since 3/17/2022
+ */
+public class GlobalController {
+
+    protected static final String API_VERSION = "v1.0";
+}
--- src/main/java/com/submarine/controller/SubmarineController.java
+++ src/main/java/com/submarine/controller/SubmarineController.java
@@ -12,28 +12,32 @@ import org.springframework.web.bind.annotation.*;
  * @since 3/16/2022
  */
 @RestController
-public class SubmarineController {
+public class SubmarineController extends GlobalController{
+
+    private static final String SUBMARINE_URL = API_VERSION + "/submarine";
+    private static final String SUBMARINE_PUT_DELETE_URL = SUBMARINE_URL + "/{id}";
 
     @Autowired
     private SubmarineService submarineService;
 
-    @PostMapping("/submarine")
+    @PostMapping(SUBMARINE_URL)
     public ResponseEntity<Submarine> saveSubmarine(@RequestBody Submarine submarine){
         return new ResponseEntity<>(submarineService.saveSubmarine(submarine), HttpStatus.CREATED);
     }
 
-    @PutMapping("/submarine")
-    public ResponseEntity<Submarine> updateSubmarine(@RequestBody Submarine submarine){
-        return new ResponseEntity<>(submarineService.updateSubmarine(submarine), HttpStatus.OK);
+    @PutMapping(SUBMARINE_PUT_DELETE_URL)
+    public ResponseEntity<Submarine> updateSubmarine(@PathVariable Long id, @RequestBody Submarine submarine){
+        Submarine s = new Submarine(id, submarine.getName(), submarine.getSubmarineClass(), submarine.getOps());
+        return new ResponseEntity<>(submarineService.updateSubmarine(s), HttpStatus.OK);
     }
 
-    @DeleteMapping("/submarine/{id}")
+    @DeleteMapping(SUBMARINE_PUT_DELETE_URL)
     public ResponseEntity<String> deleteSubmarine(@PathVariable Long id){
         submarineService.deleteSubmarine(id);
         return new ResponseEntity<>("the submarine with id " + id + "has been deleted", HttpStatus.OK);
     }
 
-    @GetMapping("/submarine")
+    @GetMapping(SUBMARINE_URL)
     public ResponseEntity<Submarine> getAllSubmarines(){
         return new ResponseEntity(submarineService.getAllSubmarines(), HttpStatus.OK);
     }
